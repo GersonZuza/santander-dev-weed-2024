df300755a878f230745867778b51d6b151933a50
GersonZuza <gersonzuza@gmail.com>
2024-07-19 16:10:58 -0300

    Configuração de banco de dados.

--- src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -22,7 +22,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> handle(NoSuchElementException notFoundException) {
-        return new ResponseEntity<>("Resourc ID not found.", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("Resource ID not found.", HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Throwable.class)
